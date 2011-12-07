528ed6828dff391871d2745221ffb55511c62908
CEXCHACK <stephan.sass@gmx.de>
2011-12-07 12:14:46 +0100

    Setup

---
+++ WumpusWorld/src/wumpusworld/solutions/ws1112/JTSBMMSSNR/Test.java
@@ -0,0 +1,14 @@
+package wumpusworld.solutions.ws1112.JTSBMMSSNR;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		exampleClass ex = new exampleClass();
+		
+	}
+
+}
---
+++ WumpusWorld/src/wumpusworld/solutions/ws1112/JTSBMMSSNR/exampleClass.java
@@ -0,0 +1,21 @@
+package wumpusworld.solutions.ws1112.JTSBMMSSNR;
+
+import model.wumpusworld.agents.AgentAction;
+import model.wumpusworld.agents.CompleteCavePerceivingAgent;
+import model.wumpusworld.environment.CompleteCavePerception;
+
+public class exampleClass extends CompleteCavePerceivingAgent {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "exampleClass";
+	}
+
+	@Override
+	protected AgentAction act(CompleteCavePerception perception) {
+		// TODO Auto-generated method stub
+		return AgentAction.WAIT;
+	}
+
+}
