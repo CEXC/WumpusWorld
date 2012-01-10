238ea563b7cf5f33d4bae68e74c25baef6b4a9cb
Nils Richter <nils.richter@uni-rostock.de>
2012-01-10 22:12:31 +0100

    habe die fortpflanzung "richtig" eingebaut.

--- WumpusWorld/src/wumpusworld/solutions/ws1112/JTSBMMSSNR/GenetischerAlgo.java
+++ WumpusWorld/src/wumpusworld/solutions/ws1112/JTSBMMSSNR/GenetischerAlgo.java
@@ -50,13 +50,42 @@ public class GenetischerAlgo {
 		
 		// Schmeisse alle ohne Berechtigung zur Fortpflanzung raus
 		int MaxEltern = Populationsgroesse*ProzentAnteilDieFortpflanzen/100;
-		ArrayList<RegelAgent> Eltern = new ArrayList<RegelAgent>(Population.subList(0, MaxEltern-1));
+		ArrayList<RegelAgent> DieBestenDerBesten = new ArrayList<RegelAgent>(Population.subList(0, MaxEltern));
+		ArrayList<RegelAgent> Eltern = new ArrayList<RegelAgent>();
 		
-				
-		for(int i=0; i<Populationsgroesse; i++) {
-			// Waehle zufaellig zwei Elternteile
-		    IRandom ZufallsZahlenGen = null;
-			ZufallsZahlenGen = SimSystem.getRNGGenerator().getNextRNG();
+		IRandom ZufallsZahlenGen = null;
+		ZufallsZahlenGen = SimSystem.getRNGGenerator().getNextRNG();
+			
+		// Jeder der besten Agenten soll min. einmal Elternteil sein
+		while(DieBestenDerBesten.size() >= 2) {
+			// Waehle zufaellig zwei der Besten	   
+			// Damit wir auch sicher zwei verschiedene Elternteile haben
+			int iVater, iMutter;
+			iMutter = ZufallsZahlenGen.nextInt(DieBestenDerBesten.size());
+			iVater = ZufallsZahlenGen.nextInt(DieBestenDerBesten.size());
+			while(iMutter == iVater) 
+				iVater = ZufallsZahlenGen.nextInt(DieBestenDerBesten.size());
+		    RegelAgent Mutter = DieBestenDerBesten.get(iMutter);
+		    RegelAgent Vater = DieBestenDerBesten.get(iVater);
+		    NeuePopulation.addAll(betreibeFortpflanzung(Mutter, Vater));
+		    Eltern.add(Mutter);
+		    Eltern.add(Vater);
+		    DieBestenDerBesten.remove(Mutter);
+		    DieBestenDerBesten.remove(Vater);	    
+	    }
+		// Sonderfall es gibt noch eine BesteDerBesten => suche ihr einen Mann!
+		if(DieBestenDerBesten.size() > 0) {
+			RegelAgent Mutter = DieBestenDerBesten.get(0);
+		    RegelAgent Vater = Eltern.get(ZufallsZahlenGen.nextInt(Eltern.size()));
+		    NeuePopulation.addAll(betreibeFortpflanzung(Mutter, Vater));
+		    Eltern.add(Vater);
+		    Eltern.add(Mutter);
+		    DieBestenDerBesten.remove(Vater);
+		    DieBestenDerBesten.remove(Mutter);
+		}
+		// Fuelle die neue Population in einer wilden Orgie auf
+		while(NeuePopulation.size() < Population.size()) {
+			// Waehle zufaellig zwei Elternteile	   
 			// Damit wir auch sicher zwei verschiedene Elternteile haben
 			int iVater, iMutter;
 			iMutter = ZufallsZahlenGen.nextInt(Eltern.size());
@@ -65,54 +94,75 @@ public class GenetischerAlgo {
 				iVater = ZufallsZahlenGen.nextInt(Eltern.size());
 		    RegelAgent Mutter = Eltern.get(iMutter);
 		    RegelAgent Vater = Eltern.get(iVater);
-
-			
-		    RegelAgent Regeler = new RegelAgent();
-		    // SituationsStatus hinzufuegen
-		    Regeler.addSituationsStatus(new WumpusVoraus());
-		    Regeler.addSituationsStatus(new WumpusGerochen());
-		    Regeler.addSituationsStatus(new WumpusGesehen());
-		    Regeler.addSituationsStatus(new GoldGesehen());
-		    Regeler.addSituationsStatus(new Gefangen());
-		    Regeler.addSituationsStatus(new NichtsFestgestellt());
-		    Regeler.addSituationsStatus(new RandGesehen());
-		 	
-		    // Hole Regeln der Eltern
-		    LinkedList<Regel> VaterRegeln = Mutter.getRegeln();
-		    LinkedList<Regel> MutterRegeln = Vater.getRegeln();
-		    
-		    // Durchlaufe alle vorhandenen Regeln und 
-		    // Kreuze Prioritaeten
-		    // ggf an zufaelliger Stelle
-		    int KS = Kreuzungsstelle;
-		    if(KS == -1)
-		    	KS = ZufallsZahlenGen.nextInt(Regeln.size());
-		    for(int j=0; j<Regeln.size(); j++) {
-		    	// Muttergenom
-		    	LinkedList<Regel> BenutzeRegeln = null;
-		    	if(j <= KS) {
-		    		BenutzeRegeln = MutterRegeln;
-		    	}
-		    	// Vatergenom
-		    	else {
-		    		BenutzeRegeln = VaterRegeln;
-		    		}
-		    	for(Regel R : BenutzeRegeln) {
-		    		if(R.VergleichOhneP(Regeln.get(j)))
-		    			Regeln.get(j).setPrioritaet(R.getPrioritaet());
-		    	}
-		    	// Ggf Mutieren wir noch eine bisl rum
-		    	if(Mutationswahrscheinlichkeit > ZufallsZahlenGen.nextInt(100))
-		    		Regeln.get(j).setPrioritaet(ZufallsZahlenGen.nextInt(1001));
-			}		
-		    Regeler.addRegeln(Regeln);
-		    NeuePopulation.add(Regeler);
+		    NeuePopulation.addAll(betreibeFortpflanzung(Mutter, Vater));    
 	    }
 		Population = NeuePopulation;
 		return true;
 	}
 	
-	
+	public ArrayList<RegelAgent> betreibeFortpflanzung(RegelAgent Mutter, RegelAgent Vater) {
+		IRandom ZufallsZahlenGen = null;
+		ZufallsZahlenGen = SimSystem.getRNGGenerator().getNextRNG();
+		RegelAgent Kind1 = new RegelAgent();
+	    RegelAgent Kind2 = new RegelAgent();
+	    // SituationsStatus hinzufuegen
+	    Kind1.addSituationsStatus(new WumpusVoraus());
+	    Kind1.addSituationsStatus(new WumpusGerochen());
+	    Kind1.addSituationsStatus(new WumpusGesehen());
+	    Kind1.addSituationsStatus(new GoldGesehen());
+	    Kind1.addSituationsStatus(new Gefangen());
+	    Kind1.addSituationsStatus(new NichtsFestgestellt());
+	    Kind1.addSituationsStatus(new RandGesehen());
+	    Kind2.addSituationsStatus(new WumpusVoraus());
+	    Kind2.addSituationsStatus(new WumpusGerochen());
+	    Kind2.addSituationsStatus(new WumpusGesehen());
+	    Kind2.addSituationsStatus(new GoldGesehen());
+	    Kind2.addSituationsStatus(new Gefangen());
+	    Kind2.addSituationsStatus(new NichtsFestgestellt());
+	    Kind2.addSituationsStatus(new RandGesehen());
+	    // Hole Regeln der Eltern
+	    LinkedList<Regel> VaterRegeln = Mutter.getRegeln();
+	    LinkedList<Regel> MutterRegeln = Vater.getRegeln();
+	    
+	    // Durchlaufe alle vorhandenen Regeln und 
+	    // Kreuze Prioritaeten
+	    // ggf an zufaelliger Stelle
+	    int KS = Kreuzungsstelle;
+	    if(KS == -1)
+	    	KS = ZufallsZahlenGen.nextInt(Regeln.size());
+	    LinkedList<Regel> RegelnKind1 = new LinkedList<Regel>();
+    	LinkedList<Regel> RegelnKind2 = new LinkedList<Regel>();
+	    for(int j=0; j<Regeln.size(); j++) {    	
+	    	if(j <= KS) {
+	    		RegelnKind1.add(new Regel(getRegel(MutterRegeln, Regeln.get(j))));
+	    		RegelnKind2.add(new Regel(getRegel(VaterRegeln, Regeln.get(j))));
+	    	}
+	    	else {
+	    		RegelnKind1.add(new Regel(getRegel(VaterRegeln, Regeln.get(j))));
+	    		RegelnKind2.add(new Regel(getRegel(MutterRegeln, Regeln.get(j))));
+	    		}
+	    	// Ggf Mutieren wir noch eine bisl rum
+	    	if(Mutationswahrscheinlichkeit > ZufallsZahlenGen.nextInt(100)) {
+	    		RegelnKind1.getLast().setPrioritaet(ZufallsZahlenGen.nextInt(1001));
+	    	}
+	    	if(Mutationswahrscheinlichkeit > ZufallsZahlenGen.nextInt(100)) {
+	    		RegelnKind2.getLast().setPrioritaet(ZufallsZahlenGen.nextInt(1001));
+	    	}
+		}		
+		Kind1.addRegeln(RegelnKind1);
+		Kind2.addRegeln(RegelnKind1);
+		ArrayList<RegelAgent> Kinder = new ArrayList<RegelAgent>();
+		Kinder.add(Kind1);
+		Kinder.add(Kind2);
+		return Kinder;
+	}
+	public Regel getRegel(LinkedList<Regel> ElternteilRegeln, Regel R) {
+		for(Regel NR : ElternteilRegeln) {
+			if(NR.VergleichOhneP(R))
+				return NR;
+		}
+		return null;
+	}
 	
 	public boolean initPopulation() {
 		if(0 == Populationsgroesse)
